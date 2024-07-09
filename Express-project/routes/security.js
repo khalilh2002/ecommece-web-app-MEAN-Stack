003233d29855e515ee97c6adf78fd1b07a01cf67
khalilh2002 <khalil.houssein2002@gmail.com>
2024-07-09 23:08:48 +0100

    email verfication front end register service no test

--- Express-project/routes/security.js
+++ Express-project/routes/security.js
@@ -2,88 +2,89 @@ const express = require("express");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const hashPassword = require("../service/hashPassword");
 
+const hashPassword = require("../service/hashPassword");
 const cookieParser = require("cookie-parser");
 const sendEmail = require("../service/sendEmail");
-const { default: mongoose } = require("mongoose");
 
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
+
 require("dotenv").config();
 
 router.use(cookieParser());
 
-
 router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email: email });
-  if (!user) {
-    return res.status(400).json({ message: "Email or password is wrong" });
-  }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+        return res.status(400).json({ message: "Email or password is wrong" });
+    }
 
-  const passwordIsValid = await bcrypt.compare(password, user.password);
-  if (!passwordIsValid) {
-    return res.status(400).json({ message: "Email or password is wrong" });
-  }
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+        return res.status(400).json({ message: "Email or password is wrong" });
+    }
 
-  const token = jwt.sign(
-    { id: user.id, email: user.email },
-    process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRE_IN }
-  );
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-  });
+    const token = jwt.sign(
+        { id: user.id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRE_IN }
+    );
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+    });
 
-  return res.status(200).json({
-    message: "the user is successfully in ",
-    user: user,
-  });
+    return res.status(200).json({
+        message: "the user is successfully in ",
+        user: user,
+    });
 });
 
-
-
 router.post("/register", async (req, res) => {
-  const { name, email, age, password } = req.body;
+    const { name, email, age, password } = req.body;
 
-  const tmpUser = await User.findOne({ email });
+    if (isEmailExist(email)) {
+        return res.status(400).json({ message: "Email is already exists" });
+    }
 
-  if (tmpUser) {
-    return res.status(400).json({ message: "Email already exists" });
-  }
+    //hash password to store it in database
+    const hashedPassword = await hashPassword(password);
 
-  const hashedPassword = await hashPassword(password);
+    //create user
+    let user = new User({
+        name: name,
+        email: email,
+        age: age,
+        password: hashedPassword,
+        role: "user",
+    });
 
+    //token to verify the email before send in it
+    const token = jwt.sign(
+        { id: user.id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: parseInt(process.env.JWT_VERIFY_EMAIL_EXPIRE_AFTER),}
+    );
 
-  let user = new User({
-    name: name,
-    email: email,
-    age: age,
-    password: hashedPassword,
-    role:'user'
-  });
+    const expireDate = parseInt(process.env.JWT_VERIFY_EMAIL_EXPIRE_AFTER);
 
-  const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
-    expiresIn: parseInt(process.env.JWT_VERIFY_EMAIL_EXPIRE_AFTER),
-  });
+    if (isNaN(expireDate)) {
+        return res
+            .status(415)
+            .json({ message: "type error in expire date from security token" });
+    }
 
-  const expireDate = parseInt(process.env.JWT_VERIFY_EMAIL_EXPIRE_AFTER);
-  if (isNaN(expireDate)) {
-    return res
-      .status(415)
-      .json({ message: "type error in expire date from security token" });
-  }
+    user.verificationToken = token;
+    user.verificationTokenExpireDate = Date.now() + expireDate * 1000;
 
-  user.verificationToken = token;
-  user.verificationTokenExpireDate = Date.now() + expireDate * 1000;
+    //style and email info
 
-  const object = "Verify Email";
-  const verifyLink = `${req.protocol}://${req.get(
-    "host"
-  )}/verify?token=${token}`;
-  const message = `
+    const object = "Verify Email";
+    const verifyLink = `${req.protocol}://${req.get('host')}/verify?token=${token}&userId=${user.id}`;
+    const message = `
       
         Hi ${user.name},
   
@@ -93,54 +94,64 @@ router.post("/register", async (req, res) => {
   
         Thanks! The [company] team
       
-      `;
+    `;
 
-  await sendEmail(email, object, message);
+    await sendEmail(email, object, message);
 
-  await user.save();
-
-  res.status(200).json({ message: "Verification email sent!" });
-});
-
-
-
-router.post("/verify/:id/:token", async (req, res) => {
-  const { id, token } = req.params;
-
-  try {
-    const user = await User.findById(id);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    if (Date.now() > user.verificationTokenExpireDate) {
-      return res.status(400).json({ message: "The link is expired" });
-    }
-
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      if (decoded.id !== user._id.toString() || decoded.email !== user.email) {
-        return res.status(400).json({ message: "Invalid verification link" });
-      }
-    } catch (error) {
-      return res.status(400).json({ message: "Invalid or expired token" });
-    }
-
-    if (user.verificationToken !== token) {
-      return res.status(400).json({ message: "Invalid verification link" });
-    }
-
-    user.verified = true;
-    user.verificationToken = null;
-    user.verificationTokenExpireDate = null;
     await user.save();
 
-    return res.status(200).json({ message: "User verified successfully" });
-  } catch (error) {
-    return res.status(500).json({ message: "Error verifying user", error });
-  }
+    res.status(200).json({ message: "Verification email sent!" });
 });
 
+router.post("/verify/:id/:token", async (req, res) => {
+    const { id, token } = req.params;
 
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        if (Date.now() > user.verificationTokenExpireDate) {
+            return res.status(400).json({ message: "The link is expired" });
+        }
+
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            if (decoded.id !== user._id.toString() || decoded.email !== user.email) {
+                return res.status(400).json({ message: "Invalid verification link" });
+            }
+        } catch (error) {
+            return res.status(400).json({ message: "Invalid or expired token" });
+        }
+
+        if (user.verificationToken !== token) {
+            return res.status(400).json({ message: "Invalid verification link" });
+        }
+
+        user.verified = true;
+        user.verificationToken = null;
+        user.verificationTokenExpireDate = null;
+        await user.save();
+
+        return res.status(200).json({ message: "User verified successfully" , user:user});
+    } catch (error) {
+        return res.status(500).json({ message: "Error verifying user", error });
+    }
+});
+
+async function isEmailExist(email) {
+    try {
+        // Check if the email already exists
+        const tmpUser = await User.findOne({ email });
+        if (tmpUser) {
+            return true;
+        }
+        return false;
+    } catch (error) {
+        console.error("Error checking email existence:", error);
+        throw error; // Re-throw the error for appropriate handling
+    }
+}
 
 module.exports = router;
